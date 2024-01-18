14f83455f389e76e41599f7fd38034fb4e02fabd
polarity <Charles.Ran9@gmail.com>
2024-01-17 23:04:53 -0500

    docs: add jsdocs for EnhancedCanvas and Game class

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -56,13 +56,27 @@
         }, interval);
     };
 
-    // Enhanced Canvas API
+    /**
+     * This class enhances the canvas API and 2D rendering context
+     * 
+     * @param canvas The HTML Canvas element to render to
+     * @param ctx The Canvas element's 2D rendering context API
+     * @param canvasWidth The width of the canvas in pixels
+     * @param canvasHeight The height of the canvas in pixels
+     */
     class EnhancedCanvas {
         canvas: HTMLCanvasElement;
         ctx: CanvasRenderingContext2D;
         canvasWidth: number;
         canvasHeight: number;
 
+        /**
+         * Constructor - populates the properties of EnhancedCanvas
+         * @param canvas The HTML Canvas element to render to
+         * @param ctx The Canvas element's 2D rendering context API
+         * 
+         * The width and height are set directly to the HTML canvas element's width and height
+         */
         constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D){
             this.canvas = canvas;
             this.ctx = ctx;
@@ -70,23 +84,49 @@
             this.canvasHeight = canvas.height;
         }
 
+        /**
+         * Getter function for the width
+         * @returns width of the canvas
+         */
         getWidth(){
             return this.canvasWidth;
         }
 
+        /**
+         * Getter function for the width
+         * @returns height of the canvas
+         */
         getHeight(){
             return this.canvasHeight;
         }
+
+        /**
+         * Getter function for the left edge's position
+         * @returns X-coordinate of the canvas' left side from the viewport boundary
+         */
         getX(){
             return this.canvas.getBoundingClientRect().x;
         }
+
+        /**
+         * Getter function for the top edge's position
+         * @returns Y-coordinate of the canvas' top side from the viewport boundary
+         */
         getY(){
             return this.canvas.getBoundingClientRect().y;
         }
+
+        /**
+         * Setter function for the canvas' dimensions, supporting high pixel densities (like retina display on Mac)
+         * @param w Width to the set the canvas to
+         * @param h Height to set the canvas to
+         */
         setHiPPICanvas(w: number, h: number) {
+            // Sets the HTML element dimensions to the given values
             this.canvasWidth = w;
             this.canvasHeight = h;
 
+            // Sets the Canvas rendering dimensions, adjusting for pixel density
             if (browser){
                 let ratio = window.devicePixelRatio;
                 this.canvas.width = w * ratio;
@@ -97,23 +137,49 @@
             }
         }
 
+        /**
+         * Sets the fill color for the rendering API
+         * @param color (destructured into r, g, b) The color to fill shapes with, in RGB
+         * @param a The transparency of the color to fill shapes with
+         */
         fill([r = 0, g = r, b = r], a = 255) {
             this.ctx.fillStyle = `rgb(${r}, ${g}, ${b}, ${a})`;
         }
 
+        /**
+         * Removes the storke for the rendering API
+         */
         noStroke() {
             this.ctx.strokeStyle = "rgb(0, 0, 0, 0)";
         }
 
+        /**
+         * Sets the stroke for the rendering API
+         * @param r Red value of the stroke
+         * @param g Green value of the stroke
+         * @param b Blue value of the stroke
+         * @param a Transparency value of the stroke
+         */
         stroke(r = 0, g = r, b = r, a = 255) {
             this.ctx.strokeStyle = `rgb(${r}, ${g}, ${b}, ${a})`;
         }
 
+        /**
+         * Draws a color and stroke filled rectangle at the given location
+         * @param x X-coordinate of the top left corner
+         * @param y Y-coordinate of the top left corner
+         * @param w Width of the rectangle
+         * @param h Height of the rectangle
+         */
         rect(x: number, y: number, w: number, h: number) {
             this.ctx.fillRect(x, y, w, h);
             this.ctx.strokeRect(x, y, w, h);
         }
 
+        /**
+         * Sets the background of the entire Canvas
+         * @param color Color in RGB to set the background to
+         */
         background([r = 255, g = r, b = r]) {
             this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
             this.fill([r, g, b]);
@@ -121,6 +187,12 @@
             this.rect(0, 0, this.canvasWidth, this.canvasHeight);
         }
 
+        /**
+         * Draws a color and stroke filled circle at the given location
+         * @param x X-coordinate of the center
+         * @param y Y-coordinate of the center
+         * @param r Radius of the circle
+         */
         circle(x: number, y: number, r: number) {
             this.ctx.beginPath();
             this.ctx.arc(x, y, r, 0, 2 * Math.PI);
@@ -360,26 +432,50 @@
         }
     }
 
+    /**
+     * This class implements the UI logic and graphical rendering of the checkers game
+     * It also implements synchronization with the server-side
+     * @param checkers The Checkers object that stores the actual game states
+     * @param canvas The EnhancedCanvas object that graphics will be rendered to
+     * @param isWhite A boolean indicating if the player is playing white (or red, otherwise)
+     * @param isTurn A boolean indicating if it is currently the player's turn
+     * @param code Room code of the game lobby
+     * @param clickedPiece Storing the coordinates of the currently selected piece (upon mouse interaction)
+     */
+
     class Game {
         checkers: Checkers;
         canvas: EnhancedCanvas;
         isWhite: boolean;
         isTurn: boolean;
         code: string;
+        clickedPiece: number[] = [];
 
+        /**
+         * Constructor
+         * Initializes the properties of the class
+         * @param canvas The EnhancedCanvas object that graphics will be rendered to
+         * @param isWhite A boolean indicating if the player is playing white (or red, otherwise)
+         * @param isTurn A boolean indicating if it is currently the player's turn
+         * @param time The starting time control for both players
+         */
         constructor(canvas: EnhancedCanvas, isWhite: boolean, code: string, time: number){
-            // 0 is empty, 1 is white, 2 is white king, 3 is red, 4 is red king
+            // initializes the properties
             this.checkers = new Checkers();
             this.canvas = canvas;
             this.isWhite = isWhite;
             playerWhite = isWhite;
             this.isTurn = !isWhite;
+
+            // sets the global variables tracking time
             myTimer = time;
             oppTimer = time;
             myTimestamp = Date.now();
             oppTimestamp = Date.now();
             myTimerString = parseTime(time);
             oppTimerString = parseTime(time);
+
+            // sets the tip message indicating whose turn it is
             if (this.isTurn){
                 gameMessage = "Your turn to move!";
                 updateMyTimer(100);
@@ -390,8 +486,6 @@
             this.code = code;
         }
 
-        clickedPiece: number[] = [];
-
         /**
          * Handle clicking a piece/cell on the canvas
          * @param e - MouseEvent of the click event
@@ -401,13 +495,20 @@
                 return;
             }
 
+            // Get the cell size of the board
             const width = this.canvas.getWidth()/8, height = this.canvas.getHeight()/8;
+
+            // Calculate the 2D array coordinates of the clicked cell
             let coordX = Math.floor((e.clientX - this.canvas.getX()) / width);
             let coordY = Math.floor((e.clientY - this.canvas.getY()) / height);
+
+            // Log this for development purposes
             console.log("Click", e.clientX, e.clientY, this.canvas.getX(), this.canvas.getY(), coordX, coordY);
 
+            // Transform the clicked coordinate into the actual board coordinate by rotating if necessary (board is flipped on white)
             let [x, y] = this.isWhite ? this.checkers.rotateCoord([coordX, coordY]) : [coordX, coordY];
 
+            // If no piece has been selected yet, assign this piece to be selected if it is a valid piece of the right color
             if (!this.clickedPiece?.length){
                 if (this.checkers.validPiece(this.isWhite, [x, y])){
                     this.clickedPiece = [x, y];
@@ -416,13 +517,17 @@
                 return;
             }
 
+            // If this piece was already selected, deselect it
             if (this.clickedPiece[0] == x && this.clickedPiece[1] == y){
                 this.clickedPiece = [];
                 this.draw();
                 return;
             }
 
+            // Find the possible moves for the player at this point
             let possMoves = this.checkers.piecePossMoves(this.isWhite, this.clickedPiece as [number, number]);
+
+            // If player is trying to move a piece somewhere, check if he is allowed to, and if so, send a request to the server to move
             if (possMoves.length && possMoves.some((el)=> {
                 return el[0] == x && el[1] == y;
             })){
@@ -436,18 +541,22 @@
         }
         /**
          * Render the board within the canvas
-         * @param (Optional) coordinate - Clicked coordinate
+         * @param (Optional) coordinate - Coordinate of clicked piece
         */
         draw(coordinate:number[] = []){
             let [coordX, coordY] = coordinate;
 
+            // Clear the canvas
             this.canvas.background([255]);
             this.canvas.noStroke();
 
+            // Get the cell dimensions
             const width = this.canvas.getWidth()/8, height = this.canvas.getHeight()/8;
 
+            // Rotate the board if necessary (board is flipped on white)
             let bd = this.isWhite ? this.checkers.rotateBoard():this.checkers.getBoard();
 
+            // If there is a clicked piece, check to see which cells that piece can move to
             let moveList: number[][] = [];
 
             if (coordinate.length){
@@ -477,6 +586,7 @@
                     // draw the cell rectangle
                     this.canvas.rect(j * width, i * height, width, height);
 
+                    // If the clicked piece can move to this cell, draw a small circle to indicate that
                     let coord = this.isWhite ? this.checkers.rotateCoord([j, i]) : [j, i];
 
                     if (coordinate.length && moveList.some((el)=> {
@@ -497,6 +607,7 @@
                         this.canvas.circle(j * width + width/2, i * height + height/2, 3 * width/8);
                     }
 
+                    // if the piece is a king, draw a ring inside the piece to indicate that
                     if (piece == 2 || piece == 4){
                         this.canvas.fill([], 0);
                         this.canvas.stroke(0);
@@ -507,13 +618,24 @@
             }
         }
 
+        /**
+         * Set the state of the board and draw it
+         * @param board Given board state
+         */
         setBoard(board: number[][]){
             this.checkers.setBoard(board);
             this.draw();
         }
 
+        /**
+         * Update the player's turn according to the given turn
+         * @param turn A boolean indicating if it is the player's turn
+         */
         setTurn(turn: boolean){
+            // Clear the clicked piece
             this.clickedPiece = [];
+
+            // If the turn has changed, pause the current running timer and start the opposite one
             let newTurn = (turn != this.isWhite);
             if (this.isTurn != newTurn){
                 myTimestamp = Date.now();
